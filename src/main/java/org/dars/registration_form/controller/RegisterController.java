12d0ce4ba5a21e4d8f461b1f6a2bd8a753795ebc
Darshan L <darshanl1254@gmail.com>
2024-12-12 13:44:42 +0530

    register controller show form, my sql applcation properties,register.html page

--- src/main/java/org/dars/registration_form/controller/RegisterController.java
+++ src/main/java/org/dars/registration_form/controller/RegisterController.java
@@ -1,5 +1,16 @@
 package org.dars.registration_form.controller;
 
+import org.dars.registration_form.dto.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RegisterController {
 
+	@GetMapping("/")
+	public String showForm(ModelMap map,Student student) {
+		map.put("student", student);
+		return "register.html";
+	}
 }
