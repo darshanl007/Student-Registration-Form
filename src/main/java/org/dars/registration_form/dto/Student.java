ab964239c585e44e725940630e3dcaf4cab5fc87
Darshan L <darshanl1254@gmail.com>
2024-12-12 13:21:47 +0530

    Student Entity Class Done

--- src/main/java/org/dars/registration_form/dto/Student.java
+++ src/main/java/org/dars/registration_form/dto/Student.java
@@ -1,5 +1,30 @@
 package org.dars.registration_form.dto;
 
-public class Student {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Entity
+@Data
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Size(min = 3, max = 15, message = "Name should be between 3 to 15 characters")
+	private String name;
+	@NotEmpty(message = "Email is Required")
+	@Email(message = "Invalid Email Format")
+	private String email;
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+	private String password;
+	@Transient
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+	private String confirmPassword;
 }
