b88b2df6e3b43f0916ebe5f0a55ca2ff20d60b71
Darshan L <darshanl1254@gmail.com>
2024-12-13 22:17:56 +0530

    Razorpay Done

--- src/main/java/org/dars/registration_form/controller/RegisterController.java
+++ src/main/java/org/dars/registration_form/controller/RegisterController.java
@@ -8,23 +8,42 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.razorpay.RazorpayException;
 
 import jakarta.validation.Valid;
 
 @Controller
 public class RegisterController {
-	
+
 	@Autowired
 	StudentService service;
 
 	@GetMapping("/")
-	public String showForm(ModelMap map,Student student) {
+	public String showForm(ModelMap map, Student student) {
 		map.put("student", student);
 		return "register.html";
 	}
-	
+
 	@PostMapping("/register")
-	public String register(@Valid Student student,BindingResult result,ModelMap map) {
-		return service.register(student,result,map);
+	public String register(@Valid Student student, BindingResult result, ModelMap map) {
+		return service.register(student, result, map);
+	}
+
+	@GetMapping("/payment")
+	public String payment() {
+		return "payment.html";
+	}
+
+	@PostMapping("/payment")
+	public String payment(@RequestParam int amount, ModelMap map) throws RazorpayException {
+		return service.payment(amount, map);
+	}
+
+	@PostMapping("/success")
+	public String success() {
+		return "payment-success.html";
 	}
 }
--- src/main/java/org/dars/registration_form/service/StudentService.java
+++ src/main/java/org/dars/registration_form/service/StudentService.java
@@ -3,10 +3,15 @@ package org.dars.registration_form.service;
 import org.dars.registration_form.dto.Student;
 import org.dars.registration_form.helper.EmailHelper;
 import org.dars.registration_form.repository.StudentRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 
 import jakarta.validation.Valid;
 
@@ -15,8 +20,7 @@ public class StudentService {
 
 	@Autowired
 	StudentRepository repository;
-	
-	
+
 	@Autowired
 	EmailHelper emailHelper;
 
@@ -38,4 +42,19 @@ public class StudentService {
 			return "register.html";
 		}
 	}
+
+	public String payment(int amount, ModelMap map) throws RazorpayException {
+
+		RazorpayClient razorpay = new RazorpayClient("rzp_test_LjeUdOv6BXQ9uv", "u8Q7IpDTKcedhm0wa14Ud4Mu");
+
+		JSONObject orderRequest = new JSONObject();
+		orderRequest.put("amount", amount * 100);
+		orderRequest.put("currency", "INR");
+
+		Order order = razorpay.orders.create(orderRequest);
+		map.put("key", "rzp_test_LjeUdOv6BXQ9uv");
+		map.put("amount", amount * 100);
+		map.put("orderId", order.get("id"));
+		return "razorpay.html";
+	}	
 }
